5a41b4d44a7b6385af0a29c7a3cab16ad628e774
Dave Foley <davidmfoley@gmail.com>
2013-12-31 09:14:55 -0800

    Break out handlers

--- index.js
+++ index.js
@@ -1,21 +1,19 @@
 var path = require('path');
 var changedRequiresByFile = require('./lib/changedRequiresByFile');
+var handleFileChanges = require('./lib/handleFileChanges');
 
 module.exports = function(options) {
   var base = process.cwd();
   var from = path.normalize(path.join(base, options._[0]));
   var to = path.normalize(path.join(base, options._[1]));
+  var handler = handleFileChanges(options['dry-run']);
 
-  changedRequiresByFile(from, to, base).forEach(function(f) {
-    if (f.from != f.to)
-      console.log(path.relative(base, f.from), ' -> ',  path.relative(base, f.to));
-    else
-      console.log(path.relative(base, f.from));
-
-    f.requires.forEach(function(r) {
-      console.log("" + r.loc.line+ ":" + r.loc.start, r.path, ' -> ', r.newPath);
-    });
-    console.log('');
-  });
+  var changes = changedRequiresByFile(from, to, base);
+  var job = {
+    from: from,
+    to: to,
+    base: base
+  };
+  handler(job, changes);
 };
 
