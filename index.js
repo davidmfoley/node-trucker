967ed930eee3206ae8f4376917a41835bbfc797a
Dave Foley <davidmfoley@gmail.com>
2014-01-02 13:50:01 -0800

    exit 1 when error occurs

--- index.js
+++ index.js
@@ -3,10 +3,17 @@ var handleFileChanges = require('./lib/handleFileChanges');
 var buildJob = require('./lib/buildJob');
 
 module.exports = function(options) {
-  var job = buildJob(options);
+  try {
+    var job = buildJob(options);
 
-  var changes = changedRequiresByFile(job);
+    var changes = changedRequiresByFile(job);
 
-  var handler = handleFileChanges(job.dryRun);
-  handler(job, changes);
+    var handler = handleFileChanges(job.dryRun);
+    handler(job, changes);
+    return 0;
+  }
+  catch (error) {
+    console.error(error.message);
+    return 1;
+  }
 };
