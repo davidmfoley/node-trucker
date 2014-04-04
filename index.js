aca14f15ca8e0244a73df89808d319cef457371d
Dave Foley <davidmfoley@gmail.com>
2014-04-03 19:34:42 -0700

    hack a dirty spike -i implementation

--- index.js
+++ index.js
@@ -24,11 +24,48 @@ function moveFiles(job) {
 }
 
 function showInfo(job) {
-  var matchingRequires = require('./lib/matchingRequires')(job);
+  var matchingRequires = require('./lib/matchingRequires');
 
   var requires = matchingRequires(job);
-  printRequires(requires);
-  throw new Error('not implemented');
+  printRequires(requires, job);
 }
 
+var path = require('path');
+function printRequires(files, job) {
+  var outbound = {}, inbound = {};
+  function digest(graph, from, to) {
+    graph[from] = graph[from] || [];
+    graph[from].push(to);
+  }
+  files.forEach(function(f) {
+    f.requires.forEach(function(r) {
+      digest(outbound, f.fullPath, r.filePath)
+      digest(inbound, r.filePath, f.fullPath)
+    });
+  });
+
+  if (job.files.length) {
+    files = files.filter(function(f) {
+      return job.files.filter(function(path) {
+        return f.fullPath.indexOf(path) === 0;
+      }).length;
+    });
+  }
+
+  files.forEach(function(f) {
+    var path = f.fullPath
+
+    print(path, '');
+    (outbound[path] || []).forEach(function(to){
+      print(to, ' ---> ');
+    });
+    (inbound[path] || []).forEach(function(from){
+      print(from, ' <--- ');
+    });
+  });
+
+  function print(file, leader) {
+    console.log(leader + path.relative(job.base, file));
+  }
+}
 
