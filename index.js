c85368416fdb4f1735cf6a1d120487692de97e29
Dave Foley <davidmfoley@gmail.com>
2014-04-03 18:28:10 -0700

    stub out info

--- index.js
+++ index.js
@@ -1,15 +1,10 @@
-var changedRequiresByFile = require('./lib/findChangedRequires');
-var handleFileChanges = require('./lib/handleFileChanges');
 var buildJob = require('./lib/buildJob');
 
 module.exports = function(options) {
   try {
     var job = buildJob(options);
-
-    var changes = changedRequiresByFile(job);
-
-    var handler = handleFileChanges(job.dryRun);
-    handler(job, changes);
+    var action = job.info ? showInfo : moveFiles;
+    action(job);
     return 0;
   }
   catch (error) {
@@ -17,3 +12,17 @@ module.exports = function(options) {
     return 1;
   }
 };
+
+
+var changedRequiresByFile = require('./lib/findChangedRequires');
+var handleFileChanges = require('./lib/handleFileChanges');
+
+function moveFiles(job) {
+  var changes = changedRequiresByFile(job);
+  var handler = handleFileChanges(job.dryRun);
+  handler(job, changes);
+}
+
+function showInfo(job) {
+  throw('not implemented');
+}
