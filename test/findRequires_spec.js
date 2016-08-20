1f5e87258f0825a8b62a78153fa92b562d4ad210
Dave Foley <davidmfoley@gmail.com>
2016-08-20 08:54:40 -0700

    es6ify test

--- test/findRequires_spec.js
+++ test/findRequires_spec.js
@@ -3,26 +3,29 @@ let expect = require('chai').expect;
 
 let findRequires = require('../lib/analyzeFiles/findRequires');
 
-describe('RequireFinder', function() {
-  describe('with es6 imports', function() {
-    it('handles a file with no requires', function() {
+describe('RequireFinder', () =>  {
+  describe('with es6 imports', () =>  {
+    it('handles a file with no requires', () =>  {
       expect(findRequires('js', "")).to.eql([]);
     });
-    it('handles a single require', function() {
+
+    it('handles a single require',  () =>  {
       var code, requires;
       code = "import * as x from './y';";
       requires = findRequires('js', code);
       expect(requires.length).to.eql(1);
       expect(requires[0].path).to.equal('./y');
     });
-    it('ignores npm modules that are required', function() {
+
+    it('ignores npm modules that are required', () =>  {
       var code, requires;
       code = "import bar from 'bar';\nimport foo from './foo' ;\n";
       requires = findRequires('js', code);
       expect(requires.length).to.eql(1);
       expect(requires[0].path).to.equal('./foo');
     });
-    it('sets location correctly', function() {
+
+    it('sets location correctly', () =>  {
       var code, req, requires;
       code = "import * as foo from './foo'";
       requires = findRequires('js', code);
@@ -32,18 +35,21 @@ describe('RequireFinder', function() {
       expect(req.loc.length).to.equal(5);
     });
   });
-  describe('with javascript', function() {
-    it('handles a file with no requires', function() {
+
+  describe('with javascript', () =>  {
+    it('handles a file with no requires', () =>  {
       expect(findRequires('js', "")).to.eql([]);
     });
-    it('handles a javascript file with a single require', function() {
+
+    it('handles a javascript file with a single require', () =>  {
       var code, requires;
       code = "var foo = require( './foo' );\n";
       requires = findRequires('js', code);
       expect(requires.length).to.eql(1);
       expect(requires[0].path).to.equal('./foo');
     });
-    it('handles a shebanged javascript file', function() {
+
+    it('handles a shebanged javascript file', () =>  {
       var code, requires;
       code = "#! /usr/bin/env node\nvar foo = require( './foo' );\n";
       requires = findRequires('js', code);
@@ -51,14 +57,16 @@ describe('RequireFinder', function() {
       expect(requires[0].path).to.equal('./foo');
       expect(requires[0].loc.line).to.equal(2);
     });
-    it('ignores npm modules that are required', function() {
+
+    it('ignores npm modules that are required', () =>  {
       var code, requires;
       code = "var bar = require('bar');\nvar foo = require( './foo' );\n";
       requires = findRequires('js', code);
       expect(requires.length).to.eql(1);
       expect(requires[0].path).to.equal('./foo');
     });
-    it('sets location correctly', function() {
+
+    it('sets location correctly', () =>  {
       var code, req, requires;
       code = "var foo = require( './foo' );\n";
       requires = findRequires('js', code);
@@ -67,27 +75,34 @@ describe('RequireFinder', function() {
       expect(req.loc.start).to.equal(21);
       expect(req.loc.length).to.equal(5);
     });
+
   });
-  describe('with coffeescript', function() {
-    describe('assigning to a variable', function() {
-      it('handles a file with no requires', function() {
+
+  describe('with coffeescript', () =>  {
+
+    describe('assigning to a variable', () =>  {
+
+      it('handles a file with no requires', () =>  {
         expect(findRequires('coffee', "")).to.eql([]);
       });
-      it('handles a coffee file with a single require', function() {
+
+      it('handles a coffee file with a single require', () =>  {
         var code, requires;
         code = "foo = require( './foo' )\n";
         requires = findRequires('coffee', code);
         expect(requires.length).to.eql(1);
         expect(requires[0].path).to.equal('./foo');
       });
-      it('ignores npm modules that are required', function() {
+
+      it('ignores npm modules that are required', () =>  {
         var code, requires;
         code = "bar = require 'bar'\nfoo = require './foo'\n";
         requires = findRequires('coffee', code);
         expect(requires.length).to.eql(1);
         expect(requires[0].path).to.equal('./foo');
       });
-      it('sets location correctly', function() {
+
+      it('sets location correctly', () =>  {
         var code, req, requires;
         code = "foo = require( './foo' )\n";
         requires = findRequires('coffee', code);
@@ -97,15 +112,17 @@ describe('RequireFinder', function() {
         expect(req.loc.length).to.equal(5);
       });
     });
-    describe('without assignment', function() {
-      it('handles requires without left hand assignment', function() {
+
+    describe('without assignment', () =>  {
+      it('handles requires without left hand assignment', () =>  {
         var code, requires;
         code = "require( './foo' )\n";
         requires = findRequires('coffee', code);
         expect(requires.length).to.eql(1);
         expect(requires[0].path).to.equal('./foo');
       });
-      it('sets location correctly', function() {
+
+      it('sets location correctly', () =>  {
         var code, req, requires;
         code = "\nrequire( './foo' )\n";
         requires = findRequires('coffee', code);
