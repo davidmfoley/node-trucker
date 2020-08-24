18d43773e622828774ee29dc0193a60f7f22676b
Dave Foley <davidmfoley@gmail.com>
2020-08-24 16:12:33 -0700

    Arrange findSourceFiles

--- src/findSourceFiles/index.ts
+++ src/findSourceFiles/index.ts
@@ -1,7 +1,7 @@
-import analyzeFiles from './analyzeFiles';
+import analyzeFiles from '../analyzeFiles';
 import fileFinder from './findFiles';
 import filterExcludedFiles from './filterExcludedFiles';
-import { TruckerJob } from './types';
+import { TruckerJob } from '../types';
 
 const findSourceFiles = (job: TruckerJob)  => {
   let files = fileFinder(job.base);
