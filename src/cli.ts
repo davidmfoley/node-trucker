7a9395aa7a50ebde2bf07ab81f665ee44d6913c1
Dave Foley <davidmfoley@gmail.com>
2022-07-17 12:59:46 -0700

    Move cli into ts src

---
+++ src/cli.ts
@@ -0,0 +1,74 @@
+import optimist from 'optimist'
+import trucker from '.'
+
+const cli = () => {
+  const options = optimist
+    .usage(
+      'Move CommonJS source files or directories without breaking your app.\nFixes requires in javascript and coffeescript files.\n$0 source [additional sources...] destination [flags]'
+    )
+    .boolean('h')
+    .alias('h', 'help')
+    .describe('h', 'Show help')
+    .boolean('i')
+    .alias('i', 'info')
+    .describe('i', 'Print dependency information for a file or path')
+    .string('f')
+    .alias('f', 'format')
+    .describe(
+      'f',
+      'Set format for info output ("default" or "dot" for graphviz-compatible dot output)'
+    )
+    .boolean('u')
+    .alias('u', 'unused')
+    .describe('u', 'Find files that are not required from any other files')
+    .boolean('m')
+    .alias('m', 'move')
+    .describe(
+      'm',
+      'Move one or more source files while fixing up any changed require paths'
+    )
+    .boolean('n')
+    .alias('n', 'dry-run')
+    .describe(
+      'n',
+      "Print out changes to be made but don't change anything (try this first!)"
+    )
+    .string('s')
+    .alias('s', 'scope')
+    .describe(
+      's',
+      'Set top "scope" directory for scanning for dependencies (default: pwd)'
+    )
+    .string('p')
+    .alias('p', 'project')
+    .describe('p', 'Set tsconfig project location (typescript only)')
+    .string('q')
+    .alias('q', 'quiet')
+    .describe('q', 'Suppress output')
+    .string('e')
+    .alias('e', 'exclude')
+    .describe('e', 'Add file glob pattern to exclude (repeatable)')
+
+  const argv = options.argv,
+    fileCount = argv._length,
+    requiredFileCount = argv.i ? 1 : 2
+
+  if (!(argv.h || argv.i || argv.m || argv.n || argv.u)) {
+    options.showHelp()
+    process.exit()
+  }
+
+  if (argv.exclude && typeof argv.exclude === 'string') {
+    argv.e = argv.exclude = [argv.exclude]
+  }
+
+  if (argv.h || fileCount < requiredFileCount) {
+    options.showHelp()
+    process.exit()
+  }
+
+  const result = trucker(argv)
+  process.exit(result)
+}
+
+export default cli
